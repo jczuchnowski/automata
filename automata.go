430f73f2600bd62a8210ffdea35577190671cacf
jczuchnowski <jakub.czuchnowski@gmail.com>
2023-11-06 19:53:53 +0100

    Use full screen width

--- automata.go
+++ automata.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+	"golang.org/x/term"
 )
 
 func main() {
@@ -11,15 +13,48 @@ func main() {
 	if len(args) == 0 {
 		panic("You have to choose a rule number between 1 and 255")
 	}
+	println("----")
+	println(int(os.Stdout.Fd()))
+	println("----")
 
-	rule, err := strconv.Atoi(os.Args[1])
+	ruleset := calculateRuleset(args[1])
+
+	fmt.Println(ruleset)
+
+	//current := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	current := firstGeneration()
+	printGeneration(current)
+
+	n := 100
+	count := 0
+
+	for count < n {
+		current = nextGeneration(current[:], ruleset)
+		count = count + 1
+		printGeneration(current)
+	}
+}
+
+func firstGeneration() []int {
+	width, _, err := term.GetSize(0)
+	if err != nil {
+		panic(err)
+	}
+
+	gen := make([]int, width)
+	a := width / 2
+	fmt.Println("index %d", a)
+	gen[width/2] = 1
+	return gen
+}
+
+func calculateRuleset(ruleNumber string) map[[3]int]int {
+	rule, err := strconv.Atoi(ruleNumber)
 	if err != nil || rule < 1 || rule > 255 {
 		panic("The rule number has to be number between 1 and 255")
 	}
 
 	ruleBinaryStr := fmt.Sprintf("%08b\n", rule)
-
-	fmt.Println(ruleBinaryStr)
 
 	chars := []rune(ruleBinaryStr)
 	var ints [8]int
@@ -29,7 +64,7 @@ func main() {
 		ints[i] = int(chars[i] - '0')
 	}
 
-	ruleMapping := map[[3]int]int{
+	return map[[3]int]int{
 		{1, 1, 1}: ints[0],
 		{1, 1, 0}: ints[1],
 		{1, 0, 1}: ints[2],
@@ -38,27 +73,6 @@ func main() {
 		{0, 1, 0}: ints[5],
 		{0, 0, 1}: ints[6],
 		{0, 0, 0}: ints[7],
-	}
-	fmt.Println(ruleMapping)
-
-	current := []int{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}
-	fmt.Println(current)
-
-	// zero1 := append(append(zero[0:1], zero[0:]...), zero[len(zero)-1:]...)
-	// for i := 0; i < len(zero1); i++ {
-	// 	slice := zero1[i : i+3]
-	// 	fmt.Println(slice)
-	// }
-
-	//next := nextGeneration(zero[:], ruleMapping)
-
-	n := 100
-	count := 0
-
-	for count < n {
-		current = nextGeneration(current[:], ruleMapping)
-		count = count + 1
-		fmt.Println(current)
 	}
 }
 
@@ -72,4 +86,16 @@ func nextGeneration(gen []int, ruleset map[[3]int]int) []int {
 		result[i] = ruleset[key]
 	}
 	return result
+}
+
+func printGeneration(gen []int) {
+	for i := 0; i < len(gen); i++ {
+		n := gen[i]
+		if n == 0 {
+			fmt.Print(" ")
+		} else {
+			fmt.Print("\u2588")
+		}
+	}
+	fmt.Print("\n")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module automata
 
 go 1.21.1
+
+require (
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+)
